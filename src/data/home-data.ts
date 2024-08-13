e499fae73417b2526f727afe514a1825e1f76e24
Sunil Kumawat <sunilkumawat6268@gmail.com>
2024-08-13 17:46:49 +0530

    Related commit(s):- add data

--- src/data/home-data.ts
+++ src/data/home-data.ts
@@ -96,6 +96,11 @@ export const HOME_DATA = [
         image:
           'https://images.unsplash.com/photo-1634833740183-3193a06cac2b?q=80&w=474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       },
+      {
+        id: 1606,
+        image:
+          'https://images.unsplash.com/photo-1634833740183-3193a06cac2b?q=80&w=474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      },
     ],
   },
 ];
